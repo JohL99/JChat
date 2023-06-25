e1c8f8b5f863e4b02ac74ab6d804d9dadf22e81a
JohL99 <jlamprec16@gmail.com>
2023-06-24 17:29:39 -0700

    initial commit, migrated to gradle

---
+++ app/src/main/java/jchat_gradle/com/obj/Node.java
@@ -0,0 +1,42 @@
+package jchat_gradle.com.obj;
+
+import jchat_gradle.com.Message;
+
+public class Node {
+
+    private Message message;
+    private Node left;
+    private Node right;
+
+    public Node(Message message) {
+        setMessage(message);;
+        left = null;
+        right = null;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    
+}
